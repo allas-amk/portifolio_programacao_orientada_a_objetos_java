5c7900a6b8f6ac8ccef19b0e7e708ac2e085100e
Allas Maycon <51690108+allas-amk@users.noreply.github.com>
2025-05-17 23:30:51 -0300

    Add files via upload

---
+++ src/main/java/br/com/engenheirodesoftwarenoespaco/defconceitos/Exerciciofaculdade.java
@@ -0,0 +1,16 @@
+package br.com.engenheirodesoftwarenoespaco.defconceitos;
+
+public class Exerciciofaculdade {
+
+    public static void main(String[] args) {
+        // Declaração de variáveis para armazenar os números
+        int numero1 = 1;
+        int numero2 = 2;
+
+        // Realiza a soma dos dois números
+        int soma = numero1 + numero2;
+
+        // Exibe o resultado na tela
+        System.out.println("A soma de " + numero1 + " e " + numero2 + " é: " + soma);
+    }
+}
